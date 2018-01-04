c175cf8a0bb521cc71748832c1024abbdec72312
Patrick Gao <patrickgao0922@gmail.com>
2018-01-04 11:24:26 +1100

    finish self registration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import express from "express"
+import ServiceRegistry from "./service-registry"
 
+let serviceRegistry = new ServiceRegistry()
 let app = express()
+
+app.put("/announce/:service/:port", (req, res, next) => {
+	let service = req.params.service
+	let port = req.params.port
+	let ip = req.connection.remoteAddress.includes("::")? `[${req.connection.remoteAddress}]`:req.connection.remoteAddress
+	serviceRegistry.add(service,ip,port)
+	res.json({result: `${service} at ${ip}:${port}`})
+})
 
 app.listen(8080,()=>{
 	console.log("Listening: 8080")
